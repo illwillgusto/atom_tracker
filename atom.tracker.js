37b83052a8868d3319ddfee7bd42948237f15ad4
Will Walton <will@endless3dge.com>
2024-04-21 14:42:20 -0500

    foreach method update
    prettier installed
    
    changed the variable name in the foreach method

--- atom.tracker.js
+++ atom.tracker.js
@@ -1,24 +1,15 @@
-$(document).ready(function() {
-
-
-  $('#form-new').submit(function(event) { // when a form is submitted run a function
-
+$(document).ready(function () {
+  $("#form-new").submit(function (event) {
+    // when a form is submitted run a function
 
     event.preventDefault();
 
     const form = $(this);
     const data = form.serialize();
 
-
-
-
     $.ajax({
-      url: 'log.php',
-      data: data
+      url: "log.php",
+      data: data,
     });
-
-
   });
-
-
 });
--- functions.php
+++ functions.php
@@ -9,21 +9,18 @@ function i($code){
 
 ?>
 
-  <?php
+<?php
 
-          $data = file_get_contents('data.json');
 
-          $array = json_decode($data, 1);
+    foreach ($data as $task) { //task is holding the information that can then be echoed
 
-          foreach ($array as $task) { //task is holding the information that can then be echoed
-
-            ?>
-          <tr>
-            <td><?= $task['name'] ?></td>
-            <td>Data</td>
-            <td>Data</td>
-            <td>Data</td>
-            <td><button class="btn btn-primary"><?= i('stop')?></button></td>
-            <td><button class="btn btn-danger"><?= i('times')?></button></td>
-            </tr>
-          <?php }?>
+?>
+    <tr>
+      <td><?= $task['name'] ?></td>
+      <td>Data</td>
+      <td>Data</td>
+      <td>Data</td>
+      <td><button class="btn btn-primary"><?= i('stop')?></button></td>
+      <td><button class="btn btn-danger"><?= i('times')?></button></td>
+    </tr>
+<?php }?>
