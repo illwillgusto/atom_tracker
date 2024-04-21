006a8f1a10180089856169cd2b200bc9dee24f1c
Will Walton <will@endless3dge.com>
2024-04-21 15:06:50 -0500

    Update functions.php
    function added for displaying the hours and minutes in a clean format

--- functions.php
+++ functions.php
@@ -7,7 +7,14 @@ function i($code){
  return $icon;
 }
 
+# Date looks cleaner with this:
 function date_nice($date) {
   return date('M j Y h:i A', $date);
+}
+
+function time_nice($seconds) {
+
+  $h = floor(($seconds/60)/60); // hours
+  $m = round(($seconds/60)) - ($h * 60); // minutes
 }
 ?>
