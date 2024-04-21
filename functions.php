cdf953df3bebead3f1311aa41cb82e1ee4030d8c
Will Walton <will@endless3dge.com>
2024-04-21 14:43:54 -0500

    table data in wrong location
    table data was added to the wrong file, moved to the log file

--- functions.php
+++ functions.php
@@ -9,18 +9,3 @@ function i($code){
 
 ?>
 
-<?php
-
-
-    foreach ($data as $task) { //task is holding the information that can then be echoed
-
-?>
-    <tr>
-      <td><?= $task['name'] ?></td>
-      <td>Data</td>
-      <td>Data</td>
-      <td>Data</td>
-      <td><button class="btn btn-primary"><?= i('stop')?></button></td>
-      <td><button class="btn btn-danger"><?= i('times')?></button></td>
-    </tr>
-<?php }?>
--- log.php
+++ log.php
@@ -4,3 +4,19 @@ $json = file_get_contents('data.json');
 $data = json_decode($json, 1);
 
 ?>
+
+<?php
+
+
+    foreach ($data as $task) { //task is holding the information that can then be echoed
+
+?>
+    <tr>
+      <td><?= $task['name'] ?></td>
+      <td>Data</td>
+      <td>Data</td>
+      <td>Data</td>
+      <td><button class="btn btn-primary"><?= i('stop')?></button></td>
+      <td><button class="btn btn-danger"><?= i('times')?></button></td>
+    </tr>
+<?php }?>
