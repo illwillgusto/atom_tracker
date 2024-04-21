6d2f3ab68a7ffdd3d0a38d70a26252865aa8cd9b
Will Walton <will@endless3dge.com>
2024-04-21 15:10:47 -0500

    Update log.php
    if statement started for the time stamp

--- log.php
+++ log.php
@@ -15,7 +15,13 @@ $data = json_decode($json, 1);
       <td><?= $task['name'] ?></td>
       <td><?= date_nice($task['date_start']) ?></td>
       <td><?= $task['date_end'] ?></td>
-      <td><?= $task['status'] ?></td>
+      <td>
+        <?
+          if($task['date_end'] == "") {
+            echo time_nice(time() -$task['date_start']); // this will determine how much time was elapse 
+          }
+        ?>
+      </td>
       <td><button class="btn btn-primary"><?= i('stop')?></button></td>
       <td><button class="btn btn-danger"><?= i('times')?></button></td>
     </tr>
