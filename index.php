99ac706262a90cdcfcfe089f7eb4ed2c24ec8e11
Will Walton <will@endless3dge.com>
2024-04-21 11:24:07 -0500

    bootstrap and font awesome
    installed bootstrap and font awesome to the project

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Atom Tracker</title>
-  <link rel="stylesheet" href="https://bootswatch.com/yeti/bootstrap.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/8716b95af6.js" crossorigin="anonymous"></script>
 </head>
 <body>
