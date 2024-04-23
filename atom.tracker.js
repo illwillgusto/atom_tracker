7c28053aaa86a603d6570a995e40295222918265
Will Walton <will@endless3dge.com>
2024-04-23 15:28:59 -0500

    Update atom.tracker.js
    created a build function
    this will allow the refresh to happen anytime we need it to

--- atom.tracker.js
+++ atom.tracker.js
@@ -1,3 +1,8 @@
+function build() {
+  $('#log').load('log.php?mode=build'); // allow us to take the data from the log.php and display it on the main page
+}
+
+
 $(document).ready(function () {
   $("#form-new").submit(function (event) {
     // when a form is submitted run a function
@@ -16,6 +21,5 @@ $(document).ready(function () {
 
   $('#tally').load('log.php?mode=tally'); //allow us to pull the data and display it in the Total Time log
 
-  $('#log').load('log.php?mode=build'); // allow us to take the data from the log.php and display it on the main page
 
 });
