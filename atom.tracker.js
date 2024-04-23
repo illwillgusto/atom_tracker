7611d2a4f742f1eda5e39ea1b2112e1a8e7a5b9f
Will Walton <will@endless3dge.com>
2024-04-23 13:55:16 -0500

    Update atom.tracker.js
    added the new mode to the ajax

--- atom.tracker.js
+++ atom.tracker.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
     const data = form.serialize();
 
     $.ajax({
-      url: "log.php",
+      url: "log.php?mode=new",
       data: data
     });
   });
 
-  $('#tally').load('log.php?mode=tally'); //allow us to pull the data and display it in the Total Time log 
+  $('#tally').load('log.php?mode=tally'); //allow us to pull the data and display it in the Total Time log
 
   $('#log').load('log.php?mode=build'); // allow us to take the data from the log.php and display it on the main page
 
